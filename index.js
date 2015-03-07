82a3c1f3d7462df3cb53e56f479f73776284a6ba
Nik Krumm <nkrumm@gmail.com>
2015-03-06 19:18:33 -0800

    Add some placeholder TODOs in app

--- index.js
+++ index.js
@@ -16,7 +16,21 @@ console.log("Watching " + args.photoDir)
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
-})
+});
+
+app.get(/photos\/[0-9]{1,}$/, function(req, res) {
+  // return the photo by ID here
+  // this is the main way the app will receive photos live.
+  res.send(req.url)
+});
+
+app.get('/photos/recent', function(req, res) {
+  // return the last N photos here
+  // this route is to be hit initially on client app connect to load latest photos.
+  // probably needs an ?offset=<int> parameter so that infinite scroll can be handled
+  res.json({})
+});
+
 
 io.on('connection', function(socket){
   console.log('Connected.');
@@ -28,6 +42,8 @@ var watcher = chokidar.watch(path.join(args.photoDir, "/*.JPG"), {
   ignored: /[\/\\]\./, persistent: true
 }).on('add', function(path) {
 	console.log(path)
+	// Todo: process photo (resize)
+	// Todo: add photo to database, and send ID via socket-io
     io.emit('new photo', path);
 });
 
