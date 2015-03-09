212c318e0521681242f6b70d90189c8b26b57823
Nik Krumm <nkrumm@gmail.com>
2015-03-08 22:04:32 -0700

    Move to external routes.js file

--- index.js
+++ index.js
@@ -10,39 +10,15 @@ var args = require('commander')
 			  .option('-d, --photo-dir [dir]', 'Directory of incoming photos to watch; default is ./', "./")
 			  .parse(process.argv);
 
-console.log("Watching " + args.photoDir)
-
-
-/* Routing */
-
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get(/photos\/IMG_[0-9]{4}.JPG$/, function(req, res) {
-  // return the photo by ID here
-  // this is the main way the app will receive photos live.
-  res.sendFile(__dirname + "/thumbs/" + path.basename(req.url));
-});
-
-app.get('/photos/recent', function(req, res) {
-  // return the last N photos here
-  // this route is to be hit initially on client app connect to load latest photos.
-  // probably needs an ?offset=<int> parameter so that infinite scroll can be handled
-  res.json({})
-});
-
+require("./routes.js")(app)
 
 io.on('connection', function(socket){
   console.log('Connected.');
 });
 
-/* Directory watch code */
-
 var sendImageToClient = function(msg){
   io.emit('new photo', msg);
 }
-
 
 var watcher = chokidar.watch(path.join(args.photoDir, "/*.JPG"), {
   ignored: /[\/\\]\./, persistent: true, ignoreInitial: true
---
+++ routes.js
@@ -0,0 +1,21 @@
+var path = require('path')
+module.exports = function(app){
+	/* Routing */
+
+	app.get('/', function(req, res) {
+	  res.sendFile(__dirname + '/index.html');
+	});
+
+	app.get(/photos\/IMG_[0-9]{4}.JPG$/, function(req, res) {
+	  // return the photo by ID here
+	  // this is the main way the app will receive photos live.
+	  res.sendFile(__dirname + "/thumbs/" + path.basename(req.url));
+	});
+
+	app.get('/photos/recent', function(req, res) {
+	  // return the last N photos here
+	  // this route is to be hit initially on client app connect to load latest photos.
+	  // probably needs an ?offset=<int> parameter so that infinite scroll can be handled
+	  res.json({})
+	});
+}
