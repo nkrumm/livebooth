bb2df8c0865abe78cf742a6486993a6e2faf488a
Nik Krumm <nkrumm@gmail.com>
2015-03-16 23:40:14 -0700

    Enable forward/back

--- src/components/photoview.jsx
+++ src/components/photoview.jsx
@@ -2,17 +2,24 @@
 var React = require('react')
 require("../main.css")
 var State = require('react-router').State;
+var Navigation = require('react-router').Navigation;
 
 module.exports = React.createClass({
     displayName: 'PhotoView',
-    mixins: [State],
+    mixins: [State, Navigation],
+    handleForward: function(){
+		this.transitionTo('photo', {id: parseInt(this.getParams().id) + 1});
+    },
+    handleBackward: function(){
+		this.transitionTo('photo', {id: parseInt(this.getParams().id) - 1});
+    },
     render: function(){
   		return (
 
   		<div id="photodetail">
-  			<div className="content"><img src={"/photo/" + this.getParams().id} width="100%"/></div>	
-			<div className="button backward">Back</div>
-			<div className="button forward">Forward</div>
+  			<div className="content"><img src={"/photo/" + this.getParams().id} /></div>	
+			<div className="button backward" onClick={this.handleBackward}>Back</div>
+			<div className="button forward" onClick={this.handleForward}>Forward</div>
   		</div>)
     },
 })
