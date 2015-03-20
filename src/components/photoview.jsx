deac1fea1e4756882c058676d4ba8d8409fe7ce9
Nik Krumm <nkrumm@gmail.com>
2015-03-19 23:45:58 -0700

    (WIP) infinite drag scroll in photodetail view

--- src/components/photoview.jsx
+++ src/components/photoview.jsx
@@ -16,11 +16,44 @@ module.exports = React.createClass({
     handleShare: function(){
 
     },
+
+    componentDidMount: function(){
+      this.opts = {
+        onSwipeEnd: function(){
+          var s = this.state.ids;
+          this.setState({
+              ids: s.concat([this.state.currentId + 2]),
+              currentId: this.state.currentId + 1
+          })
+        }.bind(this)
+      }
+      this.dragend = $(this.refs.dragend.getDOMNode()).dragend(this.opts);
+    },
+    getInitialState: function(){
+      var id = parseInt(this.getParams().id);
+      return {
+        ids: [id, id + 1],
+        currentId: id
+      }
+    },
+    componentDidUpdate: function(){
+      this.dragend = $(this.refs.dragend.getDOMNode()).dragend(this.opts);
+    }, 
     render: function(){
+      console.log("render")
+      console.log(this.dragend)
+      console.log(this.state.ids)
+      var images = this.state.ids.map(function(d){
+        return <li key={d} className="dragend-page"><img src={"/photo/" + d} /></li>
+      })
   		return (
 
   		<div id="photodetail">
-  			<div className="content"><img src={"/photo/" + this.getParams().id} /></div>
+  			<div className="content" ref="dragend">
+          <ul style={{padding: 0}}>
+            {images}
+          </ul>
+        </div>
   			<div className="footer">
 				<div className="button backward" onClick={this.handleBackward}>
 					<i className="fa fa-angle-double-left fa-lg"></i> 
